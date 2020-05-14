ca9628ba687703b6f380653fd38e156fb3935f4a
Eli Lederberg <elilederberg@Elis-MBP-2.fios-router.home>
2020-05-13 22:56:01 -0400

    json changes

--- home/work/movie-recommender/assets/js/main.js
+++ home/work/movie-recommender/assets/js/main.js
@@ -1,12 +1,12 @@
 let movies = [];
 
-const datafile = "../data.json";
+const datafile = "data.json";
 
 $(function() {
 
     //get file
     $.getJSON(datafile, function(data) {
-      console.log(data.movieSelections);
+      // console.log(data.movieSelections);
         $("input[type='button']").click(function(){
         	var genreValue = $("input[name='genre']:checked").val();
           var languageValue = $("input[name='language']:checked").val();
@@ -25,12 +25,13 @@ $(function() {
           $.each(movieData,function(i,obj){
             // console.log(obj.genre);
             if (genreValue==obj.genre &&languageValue==obj.language && timePeriodValue==obj.timePeriod && focusValue==obj.focus){
-              console.log(obj.title1, obj.director1);
+              // console.log(obj.title1, obj.director1);
+              console.log(`${i}${obj.title1} <br> ${obj.director1} ${obj.year1} ${obj.cast1} ${obj.plot1} ${obj.streaming1} <img src="${obj.poster1}">`);
               // HERE IS WHERE YOU ARE ADDING THE DIFFERENT MOVIES
-              $(".results-1").append(obj.title1 + "<br>" +  obj.director1, obj.year1, obj.cast1,obj.plot1, obj.streaming1, obj.poster1);
-              $(".results-2").append(obj.title2, obj.director2, obj.year2, obj.cast2, obj.plot2, obj.streaming2, obj.poster2);
+              $(".results-1").append(`${obj.title1} <br> ${obj.director1} <br> ${obj.year1} <br> ${obj.cast1} <br> ${obj.plot1} <br> ${obj.streaming1} <br> <img src='${obj.poster1}'> <br> <br> <br>`);
+   /*           $(".results-2").append(obj.title2, obj.director2, obj.year2, obj.cast2, obj.plot2, obj.streaming2, obj.poster2);
               $(".results-3").append(obj.title3, obj.director3, obj.year3, obj.cast3, obj.plot3, obj.streaming3, obj.poster3);
-              //put what you want to have happen with the json
+   */           //put what you want to have happen with the json
             } else {
               console.log("no match!")
             }
